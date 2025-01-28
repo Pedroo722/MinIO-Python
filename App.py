39f01ca4d1f2f01ee112cb17ee2c64d1585d32f4
Pedro Alexandre <132232273+Pedroo722@users.noreply.github.com>
2025-01-28 09:20:27 -0300

    Commit Inicial

---
+++ App.py
@@ -0,0 +1,53 @@
+from minio import Minio
+from minio.error import S3Error
+import os
+
+# Defina as credenciais e a URL do servidor MinIO
+minio_client = Minio(
+    "localhost:9000",  
+    access_key="minioaccesskey",  # Substituir pela sua chave de acesso
+    secret_key="miniosecretkey",  # Substituir pela sua chave secreta
+    secure=False  # Defomor como True se estiver usando HTTPS
+)
+
+# Nome do bucket onde os arquivos serão armazenados
+bucket_name = "meu-bucket"
+
+# criar um bucket (caso não exista)
+def create_bucket(bucket_name):
+    try:
+        if not minio_client.bucket_exists(bucket_name):
+            minio_client.make_bucket(bucket_name)
+            print(f"Bucket '{bucket_name}' criado com sucesso!")
+        else:
+            print(f"Bucket '{bucket_name}' já existe.")
+    except S3Error as e:
+        print(f"Erro ao criar o bucket: {e}")
+
+# upload de arquivo
+def upload_file(file_path, file_name):
+    try:
+        minio_client.fput_object(bucket_name, file_name, file_path)
+        print(f"Arquivo '{file_name}' carregado com sucesso!")
+    except S3Error as e:
+        print(f"Erro ao carregar o arquivo: {e}")
+
+# baixar arquivo
+def download_file(file_name, download_path):
+    try:
+        minio_client.fget_object(bucket_name, file_name, download_path)
+        print(f"Arquivo '{file_name}' baixado com sucesso para '{download_path}'!")
+    except S3Error as e:
+        print(f"Erro ao baixar o arquivo: {e}")
+
+# cria o bucket
+create_bucket(bucket_name)
+
+# Exemplo de upload de um arquivo
+file_path = "exemplo.txt"  # Caminho do arquivo que você quer fazer o upload
+file_name = "exemplo.txt"  # Nome que o arquivo terá no MinIO
+upload_file(file_path, file_name)
+
+# Exemplo de download de um arquivo
+download_path = "baixado_exemplo.txt"  # Caminho onde o arquivo será salvo
+download_file(file_name, download_path)
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# MinIO-Python
-Aplicação em Python que salva e baixa arquivos a partir do MinIO.
+# Descrição
+
+Aplicação simples em Python que salva e baixa arquivos a partir do MinIO.
+
+## Tecnologias Usadas
+
+<div align="center">
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=white)
+![MinIO](https://img.shields.io/badge/OpenCV-27338e?style=for-the-badge&logo=OpenCV&logoColor=white
+
+</div>
+
+
+## Como Executar
+### Passos para execução
+
+1. Instale o MinIO.
+   ```bash
+    python -m pip install minio
+   ```
+
+2. Execute o MinIO localmente.
+   ```bash
+    ./minio server /data --console-address ":9001"
+   ```
+
+3. Execute o arquivo python.
+   ```bash
+    python App.py
+   ```
+
+Com isso, o servidor MinIO estará na porta 9000 e o painel de administração na porta 9001.
+
+Apenas acesse o painel de administração no navegador: http://localhost:9001
+
+E use as credenciais padrão:
+
+Access Key: minioaccesskey
+Secret Key: miniosecretkey
+
+O script vai:
+- Criar um bucket chamado "meu-bucket" no MinIO (se ele ainda não existir).
+- Fazer o upload do arquivo exemplo.txt (verifique se o arquivo realmente existe no seu sistema antes de executar o upload).
+- Fazer o download do arquivo e salvar como baixado_exemplo.txt.
